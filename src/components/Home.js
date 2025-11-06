56b772de5822b9ea54eac5f2dd176ef132cc421a
Padma-darsi <125295009+Padma-darsi@users.noreply.github.com>
2025-11-06 14:51:33 +0530

    Create Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Welcome to our online store!</h2>
+      <p>Select a category to explore items.</p>
+    </div>
+  );
+};
+
+export default Home;
