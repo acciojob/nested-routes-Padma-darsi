8c9cf31b8fb3722f01dc794a6b4892e8d3c20a65
Padma-darsi <125295009+Padma-darsi@users.noreply.github.com>
2025-11-06 14:50:23 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
-
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './../styles/App.css';
+import Layout from "./Layout";
+import Home from "./Home";
+import Category from "./Category";
+import Item from "./Item";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="women" element={<Category />}>
+              <Route path=":itemId" element={<Item />} />
+            </Route>
+          </Route>
+        </Routes>
+      </Router>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
