155cec4888d3cc13fc4c46e53bd0e1d89b3aedf3
Padma-darsi <125295009+Padma-darsi@users.noreply.github.com>
2025-11-06 15:01:15 +0530

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,14 +4,14 @@ import { Link, Outlet } from "react-router-dom";
 const Layout = () => {
   return (
     <div>
-      <h1>Shop Categories</h1>
+      <h1>Categories</h1>
       <nav>
         <ul>
           <li>
             <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/women">Women</Link>
+            <Link to="/category">Women</Link>
           </li>
         </ul>
       </nav>
